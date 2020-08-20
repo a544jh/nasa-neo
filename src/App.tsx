f8eedeb7fac7d3b2a5e82ad47de35fdb8efbfff6
Axel Wikström <axel.wikstrom@gmail.com>
2020-08-20 18:47:02 +0300

    add basic data fetching and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,53 @@
-import React from "react";
-import { Header, Table, Label } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Header, Table } from "semantic-ui-react";
 import "./App.css";
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
+import { fetchNeos, NearEarthObject } from "./NeoApi";
 
 function App() {
+  const [neos, setNeos] = useState<NearEarthObject[]>([]);
+
+  useEffect(() => {
+    fetchNeos("", "").then((neos) => {
+      setNeos(neos);
+      console.log(neos);
+    });
+  }, []);
+
   return (
     <div className="App">
       <Header as="h1">Hello Semantic UI</Header>
-      <input type='date'></input>
+      <input type="date"></input>
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+            <Table.HeaderCell>Time (UTC)</Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Diameter</Table.HeaderCell>
+            <Table.HeaderCell>Miss Distance</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Label ribbon>First</Label>
-            </Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
+          {neos.map((neo) => (
+            <Table.Row key={neo.id} negative={neo.is_potentially_hazardous_asteroid}>
+              <Table.Cell>
+                {neo.close_approach_data[0].close_approach_date}
+              </Table.Cell>
+              <Table.Cell>
+                {neo.close_approach_data[0].close_approach_date_full}
+              </Table.Cell>
+              <Table.Cell>{neo.name}</Table.Cell>
+              <Table.Cell>
+                {neo.estimated_diameter.kilometers.estimated_diameter_min} -{" "}
+                {neo.estimated_diameter.kilometers.estimated_diameter_max}
+              </Table.Cell>
+              <Table.Cell>
+                {neo.close_approach_data[0].miss_distance.kilometers}
+              </Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
     </div>
---
+++ src/NeoApi.ts
@@ -0,0 +1,63 @@
+interface NeoApiResponse {
+  near_earth_objects: NeoDates;
+}
+
+interface NeoDates {
+  [date: string]: NearEarthObject[];
+}
+
+export interface NearEarthObject {
+  id: string;
+  name: string;
+  nasa_jpl_url: string;
+  close_approach_data: CloseApproachData[];
+  estimated_diameter: {
+    kilometers: EstimatedDiameter;
+    meters: EstimatedDiameter;
+    miles: EstimatedDiameter;
+    feet: EstimatedDiameter;
+  };
+  is_potentially_hazardous_asteroid: boolean;
+  is_sentry_object: boolean;
+}
+
+interface CloseApproachData {
+  close_approach_date: string;
+  close_approach_date_full: string;
+  epoch_date_close_approach: string;
+  relative_velocity: {
+    kilometers_per_second: number;
+    kilometers_per_hour: number;
+    miles_per_hour: number;
+  };
+  miss_distance: {
+    astronomical: number;
+    lunar: number;
+    kilometers: number;
+    miles: number;
+  };
+  orbiting_body: string;
+}
+
+interface EstimatedDiameter {
+  estimated_diameter_min: number;
+  estimated_diameter_max: number;
+}
+
+export async function fetchNeos(
+  startDate: string,
+  endDate: string
+): Promise<NearEarthObject[]> {
+  const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY`;
+  let response = await (await fetch(url)).json();
+  return flattenResponse(response);
+}
+
+function flattenResponse(resp: NeoApiResponse): NearEarthObject[] {
+  let flattened = [];
+  for (let date in resp.near_earth_objects) {
+    let neos = resp.near_earth_objects[date];
+    flattened.push(...neos);
+  }
+  return flattened;
+}
